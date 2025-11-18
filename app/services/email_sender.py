48fbe84400efe88b8831117c012ce3d310f0cd1a
AISHWARYA <96626999+Aishwarya8py@users.noreply.github.com>
2025-11-18 17:20:24 +0530

    Remove docstring from send_email function
    Removed docstring from send_email function.

--- app/services/email_sender.py
+++ app/services/email_sender.py
@@ -5,9 +5,7 @@ from ..config import FROM_EMAIL, SMTP_HOST, SMTP_PASS, SMTP_PORT, SMTP_USER
 
 
 def send_email(to_email: str, subject: str, body: str):
-    """
-    Send an email via SMTP(e.g., Gmail).
-    """
+    
     msg = EmailMessage()
     msg["From"] = FROM_EMAIL
     msg["To"] = to_email
